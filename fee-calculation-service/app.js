a8ab40feda0771330712eda39f358d2f7279f19b
Satenik Hovsepyan <satenik.hovsepyan@aalto.fi>
2019-04-12 21:11:10 +0300

    added fee-calculation endpoint

--- fee-calculation-service/app.js
+++ fee-calculation-service/app.js
@@ -1,6 +1,6 @@
 var express = require('express'),
   app = express(),
-  port = process.env.PORT || 3000;
+  port = process.env.PORT || 4000;
 
 const configureRoutes = require('./src/fee-calculation');
 
--- fee-calculation-service/src/fee-calculation.js
+++ fee-calculation-service/src/fee-calculation.js
@@ -2,6 +2,49 @@
 const status = require('http-status')
 const uuid = require('uuid/v1');
 
+var DoctorTypes = Object.freeze({
+    "cardiologist": 1,
+    "anesthesiologist": 2,
+    "surgeon": 3,
+    "allergist": 4,
+    "other": 5
+})
+
 module.exports = (app) => {
-    
+    app.get('/fee-calculation/fee', (req, res, next) => {
+
+        let duration = req.query.duration;
+        let type = req.query.type;
+
+        let fee = 0;
+        switch(parseInt(type)) {
+            case DoctorTypes.cardiologist:
+            {
+                fee = 200;
+                break;
+            }
+            case DoctorTypes.anesthesiologist:
+            {
+                fee = 100;
+                break;
+            }
+            case DoctorTypes.surgeon:
+            {
+                fee = 250;
+                break;
+            }
+            case DoctorTypes.allergist:
+            {
+                fee = 110;
+                break;
+            }
+            case DoctorTypes.other:
+            {
+                fee = 50;
+                break;
+            }
+
+        }
+        res.status(status.OK).json({fee: fee * duration})
+    })
 }
